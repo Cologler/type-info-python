5b783d4116ee9870b9c7600e2d0c3b882814d1a2
Cologler <skyoflw@gmail.com>
2019-04-25 23:06:34 +0800

    fixes test

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,8 +12,8 @@ import pytest
 
 @pytest.mark.skipif(sys.version_info[:3] > (3, 7, 0), reason='version 3.7.0 api')
 def test_py_370():
-    assert not hasattr(typing, 'OrderedDict')
+    pass
 
 @pytest.mark.skipif(sys.version_info[:3] > (3, 7, 1), reason='version 3.7.1 api')
 def test_py_371():
-    pass
+    assert not hasattr(typing, 'OrderedDict')
